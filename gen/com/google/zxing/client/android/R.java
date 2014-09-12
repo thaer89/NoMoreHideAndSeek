33d88102e8118431f67980b894abf41a34637a56
Esraa-AlArmouti <esraaalarmouti@gmail.com>
2014-09-13 02:19:32 +0300

    updated HideAndSeek

--- gen/com/google/zxing/client/android/R.java
+++ gen/com/google/zxing/client/android/R.java
@@ -81,16 +81,16 @@ public final class R {
 		public static final int viewfinder_view = 0x7f0a0049;
 	}
 	public static final class layout {
-		public static final int app_picker_list_item = 0x7f030018;
-		public static final int bookmark_picker_list_item = 0x7f030019;
-		public static final int capture = 0x7f03001a;
-		public static final int encode = 0x7f03001b;
-		public static final int help = 0x7f03001d;
-		public static final int history_list_item = 0x7f03001e;
-		public static final int search_book_contents = 0x7f03001f;
-		public static final int search_book_contents_header = 0x7f030020;
-		public static final int search_book_contents_list_item = 0x7f030021;
-		public static final int share = 0x7f030022;
+		public static final int app_picker_list_item = 0x7f030019;
+		public static final int bookmark_picker_list_item = 0x7f03001a;
+		public static final int capture = 0x7f03001b;
+		public static final int encode = 0x7f03001c;
+		public static final int help = 0x7f03001e;
+		public static final int history_list_item = 0x7f03001f;
+		public static final int search_book_contents = 0x7f030020;
+		public static final int search_book_contents_header = 0x7f030021;
+		public static final int search_book_contents_list_item = 0x7f030022;
+		public static final int share = 0x7f030023;
 	}
 	public static final class menu {
 		public static final int capture = 0x7f0f0000;
